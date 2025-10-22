79c7e431b634e726335858af3b67fd2897b8519f
Saket Hatwar <hatwar.saket@mcin.ca>
2025-10-22 15:37:59 -0400

    fixes

--- modules/issue_tracker/jsx/IssueTrackerBatchMode.js
+++ modules/issue_tracker/jsx/IssueTrackerBatchMode.js
@@ -14,7 +14,7 @@ import {withTranslation} from 'react-i18next';
  * @param {object} props - The component props
  * @param {object} props.options - The options for the IssueTrackerBatchMode
  */
-function IssueTrackerBatchMode(props) {
+function IssueTrackerBatchMode({options = {}, t}) {
   const [issues, setIssues] = useState([]);
   const [selectedCategories, setSelectedCategories] = useState([]);
   const [selectedPriorities, setSelectedPriorities] = useState([]);
@@ -25,7 +25,6 @@ function IssueTrackerBatchMode(props) {
   const [error, setError] = useState(null);
   const [assignees, setAssignees] = useState({});
   const [otherWatchers, setOtherWatchers] = useState({});
-  const {t} = this.props;
 
   // Pagination state
   const [page, setPage] = useState({
